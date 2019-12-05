0821b6d1c752d99b754fa7b136c327374e04d634
DarraghClarke <darragh.clarke@ucdconnect.ie>
2019-12-05 18:00:37 +0000

    load-balancer now uses random number to select gateway

--- load-balancer/src/main/java/service/balancer/Balancer.java
+++ load-balancer/src/main/java/service/balancer/Balancer.java
@@ -2,9 +2,13 @@ package service.balancer;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class Balancer {
@@ -13,8 +17,35 @@ public class Balancer {
 
     @GetMapping("/getGateway")
     public String getGateway() {
-        //this just returns the first gateway  currently
-        return gateways.get(0).toString();
+        return findGatewayToReturn();
+    }
+
+    public String findGatewayToReturn() {
+        Boolean alive = false;
+        String gatewayToReturn = null;
+        int random = new Random(System.currentTimeMillis()).nextInt(gateways.size());
+
+        try {
+            alive = checkGatewayHealth(gateways.get(random).toString());
+            gatewayToReturn = gateways.get(random).toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (!alive) {
+            //the gateway is dead so we should remove it and try again from the remaining list
+            gateways.remove(random);
+            findGatewayToReturn();
+        }
+        //if its alive return the gateway
+        return gatewayToReturn;
+    }
+
+    public Boolean checkGatewayHealth(String gateway) throws IOException {
+        String[] ipAddress = gateway.split(":");//this seperates the ip address from the port
+        InetAddress address = InetAddress.getByName(ipAddress[0]);
+        Boolean reachable = address.isReachable(10000);
+        return reachable;
     }
 
     @PostMapping("/addGateway")
